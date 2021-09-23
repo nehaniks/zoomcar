21cdd0a445cec2c7406e98a026551a9f1f0cc9f3
Neha Nikhat <nehanikhat18@gmail.com>
2021-09-23 21:02:25 +0530

    Created home content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <h1>ZoomCar</h1>
     </div>
   );
---
+++ src/components/home/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+export default function Home() {
+  return (
+    <div id="home" className="background">
+      <div className="home-content">
+        <h1 className="intro">DRIVE IN A SANITISED ZOOMCAR</h1>
+        <h3>
+          Self Drive Car Rental in <span>Bangalore</span>
+        </h3>
+        <div className="search-box">Start your wonderful journey</div>
+      </div>
+    </div>
+  );
+}
