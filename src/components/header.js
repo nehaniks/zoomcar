4e0b1c3136efa0468675bdd42586b4be7a3746a0
Neha Nikhat <nehanikhat18@gmail.com>
2021-09-23 20:12:08 +0530

    Added hover links to header

--- src/components/header.js
+++ src/components/header.js
@@ -11,10 +11,54 @@ export default function Header() {
         <div className="links">
           <ul className="links-list">
             <li>OFFERS</li>
-            <li>SUBSCRIPTION</li>
-            <li>AMIGO</li>
-            <li>ZMS</li>
-            <li>MOBILE APP</li>
+            <li>
+              SUBSCRIPTION
+              <div className="link-description">
+                <span className="logo subs-logo" />
+                <p>
+                  Hassle-free way of getting your personal car for flexible long
+                  term duration. You also have an option to share your car with
+                  other Zoomcar customers &amp; save up to 70% of the monthly
+                  subscription fee.
+                </p>
+                <div className="description-button">KNOW MORE</div>
+              </div>
+            </li>
+            <li>
+              AMIGO
+              <div className="link-description amigo-desc">
+                <span className="logo amigo-logo" />
+                <p>
+                  A unique Community Vehicle Pooling program that allows you to
+                  share your car with your community and friends while the car
+                  not in use and earn money.
+                </p>
+                <div className="description-button">KNOW MORE</div>
+              </div>
+            </li>
+            <li>
+              ZMS
+              <div className="link-description zms-desc">
+                <span className="logo zms-logo" />
+                <p>
+                  Connected technology offerings and shared mobility solutions
+                  for businesses worldwide.
+                </p>
+                <div className="description-button">KNOW MORE</div>
+              </div>
+            </li>
+            <li>
+              MOBILE APP
+              <div className="app-link">
+                <a href="#" className="app-logo google-play">
+                  <span>Google Play</span>
+                </a>
+
+                <a href="#" className="app-logo app-store">
+                  <span>App Store</span>
+                </a>
+              </div>
+            </li>
           </ul>
         </div>
 
